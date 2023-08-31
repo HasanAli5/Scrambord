ef1f142cd7ca4b2483fa09947bee8b3bfae2cf6f
Hasan <h.ali364@outlook.com>
2023-08-31 02:45:50 +0100

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className=' overflow-hidden'>
-      <body className=' relative w-screen h-screen dark:bg-stone-950 bg-stone-50 scrollbar-hide'>{children}</body>
+    <html lang="en" className=' overflow-clip'>
+      <body className=' relative w-screen h-screen dark:bg-stone-950 bg-stone-50 scrollbar-hide overflow-clip'>{children}</body>
     </html>
   )
 }
