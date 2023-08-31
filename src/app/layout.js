72af306996759cb4f5f0916a25acd3511bfdcc91
Hasan <h.ali364@outlook.com>
2023-08-31 03:50:40 +0100

    fx2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import Head from 'next/head'
 export const metadata = {
   title: 'Scrambord',
   description: "The Wordy Tile's Game",
@@ -7,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className=' overflow-clip'>
+      <Head>
+        <meta name="google-site-verification" content="VERIFICATION_ID" />
+      </Head>
       <body className=' relative w-screen h-screen dark:bg-stone-950 bg-stone-50 scrollbar-hide overflow-clip'>{children}</body>
     </html>
   )
