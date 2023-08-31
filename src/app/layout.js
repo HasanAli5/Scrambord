3b942ad627a0516bd28c45317a63850bedc0893e
Hasan <h.ali364@outlook.com>
2023-08-31 03:52:34 +0100

    fx3

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className=' overflow-clip'>
       <Head>
-        <meta name="google-site-verification" content="VERIFICATION_ID" />
+        <meta name="google-site-verification" content="5kt4kg5_0GmlZgd9sais5GZR7IevVrZAeqbGPsH7LUY" />
       </Head>
       <body className=' relative w-screen h-screen dark:bg-stone-950 bg-stone-50 scrollbar-hide overflow-clip'>{children}</body>
     </html>
