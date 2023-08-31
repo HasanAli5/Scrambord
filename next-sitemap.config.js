ee89ec9ef6c165566676ed1ba254f7094b9432b3
Hasan <h.ali364@outlook.com>
2023-08-31 05:53:41 +0100

    fixes

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: "https://scrambord.vercel.app/",
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-  }
-    
