dd6f8f54b4eb02dcc53d7a821a00fa2e072d0062
Hasan <h.ali364@outlook.com>
2023-09-01 03:09:23 +0100

    metadata update

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,28 @@ import './globals.css'
 
 export const metadata = {
   title: 'Scrambord',
-  description: "The Wordy Tiles Game",
+  siteName: 'Scrambord',
+  applicationName: 'Scrambord',
+  description: 'The Wordy Tiles Game.',
+  keywords: ['Tile', 'Board', 'Game'],
+  creator: 'Galtz',
+  type: 'website',
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+    icons: {
+      icon: '/favicon.ico',
+    },
+  },
   other: { "google-site-verification":"5kt4kg5_0GmlZgd9sais5GZR7IevVrZAeqbGPsH7LUY"}
 }
 
