25827e9e5ff77b81184a164c0afead854b056c85
Galtz <has.ali2005@hotmail.com>
2024-05-14 23:15:38 +0100

    clearer icons

--- src/app/page.js
+++ src/app/page.js
@@ -356,21 +356,25 @@ class TileStatics {
     let type = String(t)
     switch (type) {
       case "1h":
-        return "1";
+        return "-1";
       case "2h":
-        return "2";
+        return "-2";
       case "3h":
-        return "3";
+        return "-3";
       case "1p":
-        return "1";
+        return "+1";
       case "2p":
-        return "2";
+        return "+2";
       case "3p":
-        return "3";
+        return "+3";
       case "2s":
         return "2x";
       case "3s":
         return "3x";
+      case "rd":
+        return <HandRaisedIcon className="h-full sm:p-1 md:p-2 stroke-1"/>;
+      case "rbe":
+        return <PlusIcon className="h-full sm:p-1 md:p-2 stroke-1"/>;
       default:
         return "";
     }
